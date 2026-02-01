da1453c71cf4cd8ca204910c401ef43b72179880
bbianca2004 <biancapitu11@gmail.com>
2026-02-01 22:32:35 +0100

    fix: add correct paths for models

--- app/classifiers/loader.py
+++ app/classifiers/loader.py
@@ -5,7 +5,7 @@ class ModelBundle:
         self.tokenizer = AutoTokenizer.from_pretrained(path)
         self.model = AutoModelForSequenceClassification.from_pretrained(path)
 
-# Load your three models
-current_state = ModelBundle(r"C:\repos\semester_project\metafeedback_feedback_on_feedback\outputs\google_electra-base-discriminator\models\current_state_lr2e-05_bs8_ep3")
-next_steps = ModelBundle(r"C:\repos\semester_project\metafeedback_feedback_on_feedback\outputs\google_electra-base-discriminator\models\next_steps_lr1e-05_bs32_ep5")
-strategy = ModelBundle(r"C:\repos\semester_project\metafeedback_feedback_on_feedback\outputs\google_electra-base-discriminator\models\strategy_lr3e-05_bs16_ep3")
+# Load models for each rubric category
+current_state = ModelBundle(r"app\models\bert-base-uncased\current_state_lr2e-05_bs32_ep3")
+next_steps = ModelBundle(r"app\models\google_electra-base-discriminator\next_steps_lr1e-05_bs32_ep5")
+strategy = ModelBundle(r"app\models\google_electra-base-discriminator\strategy_lr3e-05_bs16_ep3")
